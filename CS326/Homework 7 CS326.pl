dc8aefea55337db9cd58d396743f456d1cfe186a
Clayton Franklin <cfranklin16@Clayton-J-Franklins-MacBook-Pro.local>
2018-04-30 21:18:36 -0700

    Finished the problems

--- CS326/Homework 7 CS326.pl
+++ CS326/Homework 7 CS326.pl
@@ -11,15 +11,19 @@ subset([],_).
 subset([H | T] , Y) :- isSet([H | T]) , isSet(Y) , member(H,Y), subset(T,Y).
 
 %Problem 1C
-%union([],_,_).
-%union(A,B,C) :- isSet(A) , isSet(B),
+union([],_,_).
+union(A,B,C) :- subset(A,C) , subset(A,B), append(A,B,List), subset(C,List), isSet(C).
 
 %Problem 1D
+intersection([],_,[]).
+intersection(_,[],[]).
+intersection([H|T], B, [C|V]) :- member(H, B), C is H, intersection(T, B, V).
+intersection([H|T], B, C) :- not(member(H,B)), intersection(T, B, C).
 
 %Problem 2
 tally(_,[],0).
 tally(A,[A|T],N) :- tally(A,T,X), N is X + 1.
-tally(A,[H|T],N) :- tally(A,T,N).
+tally(A,[_|T],N) :- tally(A,T,N).
 
 %Problem 3
 subst(_,_,[],[]).
@@ -27,7 +31,11 @@ subst(A,B,[A|T],[B|L1]) :- subst(A,B,T,L1).
 subst(A,B,[H|T],[H|L1]) :- H \= A, subst(A,B,T,L1).
 
 %Problem 4
-insert(_,[],[]).
-insert(A,[A|T],[A|L1]) :- insert(A,T,L1).
-insert(A,[H|T],[H|L1]) :- H @< A, insert(A,T,L1).
+insert(A,[],[A]).
+insert(A,[H|T],[R|Q]) :- H < A, R is H, insert(A,T,Q).
+insert(A,[H|T],[R|Q]) :- H > A, R is A, append([H],T,Q).
+
 %Problem 5
+flatten([],[]).
+flatten([H|T],L) :- flatten(H,D), flatten(T,E), append(D,E,L).
+flatten(A,[A]).
